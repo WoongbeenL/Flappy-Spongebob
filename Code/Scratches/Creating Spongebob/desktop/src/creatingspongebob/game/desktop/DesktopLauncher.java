140d1331f761b3046364e4ecab8ab4fa4a3b2ef0
Unknown <ajyagnik28@gmail.com>
2018-06-05 10:22:45 -0400

    Added Spongebob image

---
+++ Code/Scratches/Creating Spongebob/desktop/src/creatingspongebob/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package creatingspongebob.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import creatingspongebob.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+
+	}
+}
+
+
+
