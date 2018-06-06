fd9ffcc5625ab04b20302b7d5af4e2a036f07ffc
Unknown <ultrawoong2@gmail.com>
2018-06-06 10:32:40 -0400

    Revert "Created Basic Scratch"
    This reverts commit ffa16105c74c93c96f429d9fc6d2c8a2b43bd727.

--- Code/Scratches/Background Test/core/src/gdx/game/Main.java
+++ Code/Scratches/Background Test/core/src/gdx/game/Main.java
@@ -6,15 +6,20 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Main extends ApplicationAdapter {
     SpriteBatch batch;
-    
+    Texture img;
+    Sprite sprite;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        sprite = new Sprite(img);
+        sprite.setPosition(175, 150); //coordinates of image
+        sprite.setRotation(90f); //rotation
+        sprite.setScale(1.5f); //bigger or smaller
     }
 
     @Override
@@ -22,6 +27,8 @@ public class Main extends ApplicationAdapter {
         Gdx.gl.glClearColor(45 / 255f, 233 / 255f, 255 / 255f, 1); //colour
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
+        batch.draw(sprite, sprite.getX(), sprite.getY()); //prints out the image
+
         batch.end();
     }
 
@@ -29,5 +36,6 @@ public class Main extends ApplicationAdapter {
     @Override
     public void dispose() {
         batch.dispose();
+        img.dispose(); //gets rid of it
     }
 }
