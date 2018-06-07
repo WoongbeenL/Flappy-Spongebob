3b2db13c4b476388b236092845a5123dac3b5715
Unknown <ultrawoong2@gmail.com>
2018-06-07 10:33:05 -0400

    Created Release 1.0

--- Code/Releases/Version 1.0/core/src/gdx/game/Main.java
+++ Code/Releases/Version 1.0/core/src/gdx/game/Main.java
@@ -6,13 +6,16 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Main extends ApplicationAdapter {
     SpriteBatch batch;
+    TextureRegion trBackground;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        trBackground = new TextureRegion(new Texture("background.png"), 0, 0, 700, 525);
     }
 
     @Override
@@ -20,6 +23,8 @@ public class Main extends ApplicationAdapter {
         Gdx.gl.glClearColor(45 / 255f, 233 / 255f, 255 / 255f, 1); //colour
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
+        batch.draw(trBackground, 0, 0);
+        batch.draw(trBackground, 0, Gdx.graphics.getHeight());
         batch.end();
     }
 
---
+++ Code/Scratches/Background/core/src/gdx/game/Main.java
@@ -0,0 +1,35 @@
+package gdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Main extends ApplicationAdapter {
+    SpriteBatch batch;
+    TextureRegion trBackground;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        trBackground = new TextureRegion(new Texture("background.png"), 0, 0, 700, 525);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(45 / 255f, 233 / 255f, 255 / 255f, 1); //colour
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(trBackground, 0, 0);
+        batch.draw(trBackground, 0, Gdx.graphics.getHeight());
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
